eb87d91d58ec52d830277deb07cede56d775d08e
ravishansenevirathna <travishan55@gmail.com>
2024-11-20 11:34:36 +0530

    2nd com

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "math"
 
 func main(){
 	var investmentAmount = 1000;
@@ -8,5 +9,19 @@ func main(){
 	var years = 10;
 
 	var futureValue = (((investmentAmount/100)*int(expectedReturnRate))*years)+investmentAmount
-	fmt.Print(futureValue)
+	fmt.Println(futureValue);
+	//power of 
+	fmt.Println(math.Pow(2,3));
+
+	var x int =5;
+	var y float64 = 99;
+	println("x is :",x);
+	println("y is :",y);
+
+
+	//go refered variable declaration
+	// this is the go recommended shorter way to assign a variable
+	testMarks := 85
+	println("your mark is :",testMarks);
+
 }
