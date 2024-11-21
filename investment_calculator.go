4a59354d258a321f75cb339500de2582a9012382
ravishansenevirathna <travishan55@gmail.com>
2024-11-21 07:06:32 +0530

    3rd commit

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,27 +1,56 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
-func main(){
-	var investmentAmount = 1000;
-	var expectedReturnRate = 5.5;
-	var years = 10;
+func main() {
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
 
-	var futureValue = (((investmentAmount/100)*int(expectedReturnRate))*years)+investmentAmount
-	fmt.Println(futureValue);
-	//power of 
-	fmt.Println(math.Pow(2,3));
+	var futureValue = (((investmentAmount / 100) * int(expectedReturnRate)) * years) + investmentAmount
+	fmt.Println(futureValue)
+	//power of
+	fmt.Println(math.Pow(2, 3))
 
-	var x int =5;
-	var y float64 = 99;
-	println("x is :",x);
-	println("y is :",y);
-
+	var x int = 5
+	var y float64 = 99
+	fmt.Println("x is :", x)
+	fmt.Println("y is :", y)
 
 	//go refered variable declaration
 	// this is the go recommended shorter way to assign a variable
 	testMarks := 85
-	println("your mark is :",testMarks);
+	fmt.Println("your mark is :", testMarks)
+
+	const yearTaxRate = 5.2
+	fmt.Println("constance is defined here", yearTaxRate)
+
+	//input and output
+	//In Go, you must pass a pointer (using the & operator) to the fmt.Scan() 
+	//function because it needs to modify the value of the variable where the input is stored. 
+
+	var username string
+	print("enter your name : ")
+	fmt.Scan(&username)
+	fmt.Println("your name is : ",username);
+
+	// get the two user input as number and print the maximum number.
+
+	var num1 int
+	var num2 int
+	fmt.Print("Enter your First number : ")
+	fmt.Scan(&num1)
+	fmt.Print("Enter your Second number : ")
+	fmt.Scan(&num2)
+	if num1 > num2 {
+		fmt.Println("Higest Number is :",num1)
+	}else{
+		fmt.Println("Higest Number is :",num2)
+	}
+	
+
 
 }
