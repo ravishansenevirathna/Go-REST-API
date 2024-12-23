5f01cf04b29bdd07343acabb3a9fbe040f368f66
ravishansenevirathna <travishan55@gmail.com>
2024-12-23 20:25:51 +0530

    liked songs feature updated

--- controller/LikedSongController.go
+++ controller/LikedSongController.go
@@ -16,7 +16,6 @@ func NewLikedSongController(service *service.LikedSongService) *LikedSongControl
 	return &LikedSongController{service: service}
 }
 
-// Endpoint to like a song
 func (c *LikedSongController) LikeSong(ctx *gin.Context) {
 	var likeSongDto dto.LikeSongRequestDto
 	if err := ctx.ShouldBindJSON(&likeSongDto); err != nil {
@@ -33,26 +32,20 @@ func (c *LikedSongController) LikeSong(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "Song liked successfully"})
 }
 
-// Endpoint to get liked songs
 func (c *LikedSongController) GetLikedSongs(ctx *gin.Context) {
-	// Get userID as a string from the URL parameter
 	userIDStr := ctx.Param("userID")
 
-	// Convert userID to an integer
 	userID, err := strconv.Atoi(userIDStr)
 	if err != nil {
-		// Handle the case where the conversion fails
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
 		return
 	}
 
-	// Call the service method with the converted userID
 	likedSongs, err := c.service.GetLikedSongsByUser(userID)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Return the liked songs as a response
 	ctx.JSON(http.StatusOK, likedSongs)
 }
